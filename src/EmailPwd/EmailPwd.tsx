3097bba1e8542be4c52a4b42ec49ee98749d4120
rasg <rodolfo@mutalis.com>
2017-01-25 18:31:48 -0600

    Revert "Displays only the username when making a Get request to fetch the email addresses from the backend"
    This reverts commit 797aefc3bf6de55c9bee1e7b46e0f5b73bafb4d2.

--- src/EmailPwd/EmailPwd.tsx
+++ src/EmailPwd/EmailPwd.tsx
@@ -30,7 +30,7 @@ export default class EmailPwd extends React.Component<IEmailPwdProps, IEmailPwdS
     };
     // Defaults
     axios.defaults.baseURL = 'http://api.example.com:3000';
-    axios.defaults.headers.common['Authorization'] = 'Token token=1c3befd671a274b0e0fd6b589edfeeb6';
+    axios.defaults.headers.common['Authorization'] = 'Token token=68df92d83bac0c0745c03f93884ca229';
   }
 
   handleError(error: AxiosError) {
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from 'react-dom';
 import { EmailBox } from './EmailBox/EmailBox';
 
 ReactDOM.render(
-    <EmailBox domainId="1" domainName="example.com"/>,
+    <EmailBox domainId="bc8dabe3-0f41-4d23-a59c-bf52173ac305" domainName="example.com"/>,
     document.getElementById('root')
 );
