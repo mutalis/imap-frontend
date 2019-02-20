b4beced9d929e28cc35f1b03b2b4dcbb8dc52968
rasg <rodolfo@mutalis.com>
2019-02-20 12:39:20 -0600

    Removes unused function parameter

--- src/emailBox.js
+++ src/emailBox.js
@@ -9,14 +9,13 @@ export default ({domainName}) => {
     getEmails(domainName)
   }, [emails] )
 
-  const getEmails = (domainName, handleStatusChange) => {
+  const getEmails = (domainName) => {
     const emails = [{id: 1, username: 'user 1', quota: 10}, {id: 2, username: 'user 2', quota: 20}]
     setEmails(emails)
     return emails
   }
 
   const emailComponents = () => emails.map(email => <Email key={email.id} username={email.username} quota={email.quota} />)
-  
   return (
     <EmailList>
       {emailComponents()}
