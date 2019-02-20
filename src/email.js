932f0a6a6399a086668d2f3827828fc8ede5d641
rasg <rodolfo@mutalis.com>
2019-02-20 12:41:53 -0600

    Refactors email component

--- src/email.js
+++ src/email.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
-export default ({username='user name', quota=0}) => {
-  return (
+export default ({username='user name', quota=0}) => (
     <tr>
       <td data-testid="username">{username}</td>
       <td>{quota}</td>
     </tr>
-  )
-}
+)
