38301ff8d709bc289dc753ff03bd27c994c70137
rasg <rodolfo@mutalis.com>
2019-07-04 21:20:43 -0500

    removes unused code

--- src/app.js
+++ src/app.js
@@ -1,29 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import { EmailBox } from './emailBox'
 
 import './app.css'
 
 const app = () => { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/app.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React v{React.version}
-        </a>
-        <EmailBox domainName='test.net' />
-      </header>
-    </div>
-  );
+    <EmailBox domainName='test.net' />
+  )
 }
 
 export default app
--- src/app.test.js
+++ src/app.test.js
@@ -4,5 +4,5 @@ import App from './app'
 
 it('renders Learn React message', () => {
   const { getByText } = render(<App />)
-  expect(getByText(`Learn React v${React.version}`)).toBeInTheDocument()
+  expect(getByText('Username')).toBeInTheDocument()
 })
