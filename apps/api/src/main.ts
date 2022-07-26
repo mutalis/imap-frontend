13c06d4eaddc2fee0e936566f581a0735df04197
rasg <rodolfo@mutalis.com>
2022-07-26 12:38:49 -0500

    Add API endpoint to search email accounts by the username.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,6 +8,12 @@ app.get('/emails', (_, res) => {
   res.send(emails);
 });
 
+app.get('/search', (req, res) => {
+
+  const queryString = ((req.query.q as string) ?? '').toLowerCase();
+  res.send(emails.filter(({username}) => username.toLowerCase().includes(queryString)));
+});
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
