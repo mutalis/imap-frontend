70ac51ce3ba90ad2d578325fd811c80aaf4856f4
rasg <rodolfo@mutalis.com>
2022-07-26 18:02:09 -0500

    Add CORS feature to the Api App.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,9 @@
 import * as express from 'express';
-
+import * as cors from 'cors';
 import { emails } from './emails'
 
 const app = express();
-
+app.use(cors());
 app.get('/emails', (_, res) => {
   res.send(emails);
 });
