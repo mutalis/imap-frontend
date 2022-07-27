bae016eeca1b35b6de12d826e2fd6f17b54ede2f
rasg <rodolfo@mutalis.com>
2022-07-26 20:30:56 -0500

    Add Email interface to the shared-types library

--- libs/shared-types/src/lib/shared-types.ts
+++ libs/shared-types/src/lib/shared-types.ts
@@ -2,3 +2,9 @@ export function sharedTypes(): string {
   return 'shared-types';
 }
 
+export interface Email {
+  id:       string;
+  username: string;
+  quota:    number;
+  domainId: string;
+}
