2574bef2a001fb97658fba53de2422307d539d92
rasg <rodolfo@mutalis.com>
2022-07-27 21:35:09 -0500

    Remove old App.

--- src/setupTests.js
+++
@@ -1,6 +0,0 @@
-// react-testing-library renders your components to document.body,
-// this will ensure they're removed after each test.
-// this is basically: afterEach(cleanup)
-import '@testing-library/react/cleanup-after-each'
-// this adds jest-dom's custom assertions
-import 'jest-dom/extend-expect'
