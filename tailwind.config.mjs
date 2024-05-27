7d08fd2263aa71f67cabce62c7545cefd3ada556
rasg <rodolfo@mutalis.com>
2024-05-27 15:50:40 -0600

    adds NextUI components(React Aria) to Astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
