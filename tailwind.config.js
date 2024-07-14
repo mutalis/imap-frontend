831013165ab291eee2b286de7328eb1a84a3322a
rasg <rodolfo@mutalis.com>
2024-07-14 13:49:08 -0600

    NextUI setup, all the components are imported from a single package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
