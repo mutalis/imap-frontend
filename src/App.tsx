da72a8fa2c12dba2dcc1736e837f49491048dfa3
rasg <rodolfo@mutalis.com>
2024-08-07 20:28:47 -0600

    adds the NextUI Table component as a demo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,116 @@
 import "./App.css";
 import { NextUIProvider } from "@nextui-org/react";
-import { Button } from "@nextui-org/react";
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  User,
+  Chip,
+  Tooltip,
+  ChipProps,
+  getKeyValue,
+} from "@nextui-org/react";
+import { EditIcon } from "./EditIcon";
+import { DeleteIcon } from "./DeleteIcon";
+import { EyeIcon } from "./EyeIcon";
+import { columns, users } from "./data";
 
-const App = () => {
-  return (
-    <NextUIProvider>
-      <Button color="primary">Button</Button>
-    </NextUIProvider>
-  );
+const statusColorMap: Record<string, ChipProps["color"]> = {
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
 };
 
-export default App;
+type User = (typeof users)[0];
+type HearderTitle = (typeof columns)[0];
+
+export default function App() {
+  const renderCell = React.useCallback((user: User, columnKey: React.Key) => {
+    const cellValue = user[columnKey as keyof User];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: user.avatar }}
+            description={user.email}
+            name={cellValue}
+          >
+            {user.email}
+          </User>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">
+              {user.team}
+            </p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip
+            className="capitalize"
+            color={statusColorMap[user.status]}
+            size="sm"
+            variant="flat"
+          >
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
+  return (
+    <NextUIProvider>
+      <Table aria-label="Example table with custom cells">
+        <TableHeader columns={columns}>
+          {(columnName: HearderTitle) => (
+            <TableColumn
+              key={columnName.uid}
+              align={columnName.uid === "actions" ? "center" : "start"}
+            >
+              {columnName.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={users}>
+          {(item: User) => (
+            <TableRow key={item.id}>
+              {(columnKey) => (
+                <TableCell>{renderCell(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </NextUIProvider>
+  );
+}
---
+++ src/data.d.ts
@@ -0,0 +1,2 @@
+export declare const columns: any;
+export declare const users: any;
